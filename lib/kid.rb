a72bf43502dfa914b27773efed6d3226f2769abc
drubaby <peryton6@gmail.com>
2018-09-02 13:36:06 -0400

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::Instance Methods #include adds functionality via instance methods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
